f42e118581480af0be74f900112516f581bee459
agustyawan-arif <arifagustyawan@gmail.com>
2024-06-10 16:02:07 +0700

    change into langchain

--- src/qa_processor.py
+++ src/qa_processor.py
@@ -1,46 +1,54 @@
 import os
-import google.generativeai as genai
-from src.embedding_function import GeminiEmbeddingFunction
-import sys
+from langchain import PromptTemplate
+from langchain_chroma import Chroma
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
 
-path_this_file = os.path.dirname(os.path.abspath(__file__))
-pat_project_root = os.path.join(path_this_file, "..")
-sys.path.append(pat_project_root)
+os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
+
+from src.collection_management import CollectionManagement
 
 class QAProcessor:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self):
+        self.cm = CollectionManagement()
+        self.chat_model = ChatGoogleGenerativeAI(model="gemini-pro")
+        self.embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        self.create_prompt()
+        self.create_retriever()
+        self.create_rag_chain()
+    
+    def create_prompt(self):
+        template = """You are a helpful and informative bot that answers questions using text from the reference context included below. \
+            Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
+            Your answer must be in Bahasa Indonesia and always answer with friendly tone.\
+            If the passage is irrelevant to the answer, you may ignore it.
+            QUESTION: '{question}'
+            CONTEXT: '{context}'
+                
+            ANSWER:"""
 
-    def make_rag_prompt(self, query, relevant_context):
-        escaped = relevant_context.replace("'", "").replace('"', "").replace("\n", " ")
-        prompt = ("""You are a helpful and informative bot that answers questions using text from the reference context included below. \
-        Be sure to respond in a complete sentence, being comprehensive, including all relevant background information. \
-        Your answer must be in Bahasa Indonesia and always answer with friendly tone.\
-        If the passage is irrelevant to the answer, you may ignore it.
-        QUESTION: '{query}'
-        CONTEXT: '{relevant_context}'
-        
-        ANSWER:
-        """).format(query=query, relevant_context=escaped)
-        
-        return prompt
+        self.prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+    
+    def create_retriever(self):
+        langchain_chroma = Chroma(
+            client=self.cm.chroma_client,
+            collection_name="linkaja",
+            embedding_function=self.embedding,
+        )
 
-    def get_relevant_context(self, query, n_results):
-        context = self.db.query(query_texts=[query], n_results=n_results)#['documents'][0]
-        return context
-        
-    def generate_answer(self, prompt):
-        gemini_api_key = os.getenv("GEMINI_API_KEY")
-        genai.configure(api_key=gemini_api_key)
-        model = genai.GenerativeModel('gemini-pro')
-        answer = model.generate_content(prompt)
-        return answer.text
-
-    def generate_final_answer(self, query):
-        relevant_info = self.get_relevant_context(query, n_results=3)
-        relevant_text = relevant_info["documents"][0]
-        relevant_context = "\n".join(x["answer"] for x in relevant_info["metadatas"][0])
-        prompt = self.make_rag_prompt(query,relevant_context=relevant_context)
-        answer = self.generate_answer(prompt)
-        
-        return answer, relevant_info
+        self.retriever = langchain_chroma.as_retriever(k=3)
+    
+    def create_rag_chain(self):
+        self.rag_chain = (
+            {"context": self.retriever, "question": RunnablePassthrough()}
+            | self.prompt
+            | self.chat_model
+            | StrOutputParser()
+        )
+    
+    def invoke(self, question):
+        return self.rag_chain.invoke(question)
